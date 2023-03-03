20b8794b5820c4a98bc6c17c7fdf1370cb8534d6
sethtara <yeharuto@gmail.com>
2023-03-03 15:08:40 +0545

    added weather app

---
+++ weather/assets/script.js
@@ -0,0 +1,44 @@
+const api='53d1fe82cd7d5d1d915f11c304da204e';
+let searchBtn = document.getElementById("search-btn");
+let cityname = document.getElementById("city");
+let res = document.getElementById('name');
+
+
+let getWeather = () => {
+
+  let cityValue = cityname.value;
+  //incase of empty field
+  if (cityValue.length == 0) {
+    cityname.placeholder="Search City";
+  }
+  else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${api}&units=metric`;
+    cityname.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        res.textContent=`${data.name}, ${data.sys.country}`;
+
+        document.getElementById('temp').innerHTML=`${data.main.temp.toFixed(0)}&#176;C`;
+
+        document.getElementById('feels').innerHTML=`feels like: ${data.main.feels_like}&#176;C &emsp; wind: ${data.wind.speed} m/s`;
+        document.getElementById('img').innerHTML=`<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
+        document.getElementById('max').innerHTML=`max-temp : ${data.main.temp_max.toFixed(1)}&#176;C`;
+        document.getElementById('min').innerHTML=`min-temp : ${data.main.temp_min.toFixed(1)}&#176;C`;
+        document.getElementById('hum').innerHTML=`humidity : ${data.main.humidity} %`;
+        document.getElementById('pre').innerHTML=`pressure : ${data.main.pressure} hPa`;
+        cityname.placeholder="Search City";
+
+
+
+
+       })
+      //for invalid city or any error's
+      .catch(() => {
+        cityname.placeholder="Not a city !!";
+      });
+  }
+};
+searchBtn.addEventListener("click", getWeather);
+window.addEventListener("load", getWeather);
